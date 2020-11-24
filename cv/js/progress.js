dac1e2c8c060dfcc04abfd91b1283e9751ea40ea
Iosif <yaiosifua@gmail.com>
2020-11-24 03:06:50 +0200

    new style and header burger

--- cv/js/progress.js
+++ cv/js/progress.js
@@ -1,10 +1,19 @@
 
 
-window.onscroll = function() {myFunction()};
+window.onscroll = function() {scrollProgress()};
 
-function myFunction() {
+function scrollProgress() {
   var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
   var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
   var scrolled = (winScroll / height) * 100;
   document.getElementById("progress").style.width = scrolled + "%";
 }
+
+const body = document.querySelector("body");
+const burger = document.querySelector("#burger");
+const headerMenu = document.querySelector(".header__menu");
+burger.onclick = () => {
+	burger.classList.toggle('active');
+	headerMenu.classList.toggle('active');
+	body.classList.toggle('lock');
+};
