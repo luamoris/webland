67f030f1b53da5d219d8f296a417abba7b4423bb
Iosif <yaiosifua@gmail.com>
2021-04-11 22:07:56 +0300

    webland init

--- README.md
+++ README.md
@@ -1,2 +1,69 @@
-# ioland
-My development space.
+# WEBLand
+
+> This is the open source code of my portfolio. \
+> Thanks for attention.
+
+### Links
+
+> + [Source Code][sourse-code]
+> + Live Site [`iosig.fun`][iosig.fun]
+
+[sourse-code]: https://github.com/luamoris/webland
+[iosig.fun]: https://iosif.fun/
+
+
+
+### Prerequisites
+
+> #### Node.js (^12.18.3, or >=14.0.0)
+
+
+---
+
+
+### Run Сode
+
+#### Get `started`
+
+```bash
+	git clone https://github.com/luamoris/webland.git
+	cd webland
+	npm i
+	npm start
+```
+
+#### For `build`
+
+```bash
+	npm run dev 	// development
+	npm run build	// production
+```
+
+#### For `lint`
+
+> - [`stylelint`][stylelint]
+> - [`eslint`][eslint]
+
+```bash
+	npm run lint		// lint Javascript files
+	npm run stylelint	// lint SCSS files
+```
+
+
+### Author
+
+<p align="left">
+	<a href="https://github.com/luamoris">
+		<img width="50" src="https://avatars.githubusercontent.com/u/70754306?s=460&u=922c28870849f7c9528034f0512e69fb77339c84&v=4 alt="Iosif Luamoris"/>
+	</a>
+</p>
+
+
+---
+
+
+[stylelint]: https://stylelint.io/
+[eslint]: https://eslint.org/
+
+[logo]: https://raw.githubusercontent.com/luamoris/webpack-boilerplate/276b2451ca1f7d11d6cddb83ae3418105511396b/src/img/box.svg
+[github]: https://github.com/luamoris/webpack-boilerplate
---
+++ webpack.config.js
@@ -0,0 +1,250 @@
+// ======================================================================================
+
+const path = require('path');
+
+// Files
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+
+// Minify
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+const ImageminPlugin = require('imagemin-webpack-plugin').default;
+const BabelMinifyPlugin = require('babel-minify-webpack-plugin');
+const TerserWebpackPlugin = require('terser-webpack-plugin');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+
+// ======================================================================================
+
+const MODE = {
+	CURRENT: process.env.NODE_ENV,
+	isDEVELOPMENT: process.env.NODE_ENV === 'development',
+	isPRODUCTION: process.env.NODE_ENV === 'production',
+};
+
+// ======================================================================================
+
+// Resolve
+const resolvePath = (relativePath) => path.resolve(__dirname, relativePath);
+
+// Filename
+const getFilePath = (dir, title, exp) => `${dir}/${exp}/${MODE.isPRODUCTION ? `${title}.[contenthash:8].${exp}` : MODE.isDEVELOPMENT && `${title}.${exp}`}`;
+const getFileChunk = (dir, title, exp) => `${dir}/${exp}/${MODE.isPRODUCTION ? `${title}.[contenthash:8].chunk.${exp}` : MODE.isDEVELOPMENT && `${title}.chunk.${exp}`}`;
+
+// ======================================================================================
+
+const PATH = {
+	ENTRY: {
+		MAIN: resolvePath('src/app.js'),
+		BABEL: '@babel/polyfill',
+	},
+	INPUT: {
+		HTML: resolvePath('src/index.html'),
+		IMG: resolvePath('src/img/'),
+	},
+	OUTPUT: {
+		DIR: resolvePath('dist'),
+		HTML: 'index.html',
+		JS: {
+			NAME: getFilePath('static', 'bundle', 'js'),
+			CHUCK: getFileChunk('static', 'bundle', 'js'),
+		},
+		CSS: {
+			TYPE: 'text/css',
+			NAME: getFilePath('static', 'style', 'css'),
+			CHUCK: getFileChunk('static', 'style', 'css'),
+			PUBLIC: '../../',
+		},
+		IMG: 'img/',
+		MEDIA: 'assets/',
+		FONTS: 'fonts/',
+	},
+};
+
+const TEST = {
+	SCSS: /\.s[ac]ss$/,
+	IMG: /\.(png|jpe?g|svg|gif)$/,
+	FONTS: /\.(woff|woff2|ttf|eot)(\?v=\d+\.\d+\.\d+)?$/,
+	JS: /\.js$/,
+};
+
+// ======================================================================================
+
+// PLUGINS
+const getPlugins = () => {
+	const htmlWebpack = {
+		template: PATH.INPUT.HTML,
+		filename: PATH.OUTPUT.HTML,
+	};
+	MODE.isPRODUCTION ? htmlWebpack.minify = {
+		removeComments: true,
+		collapseWhitespace: true,
+		removeRedundantAttributes: true,
+		useShortDoctype: true,
+		removeEmptyAttributes: true,
+		removeStyleLinkTypeAttributes: true,
+		keepClosingSlash: true,
+		minifyJS: true,
+		minifyCSS: true,
+		minifyURLs: true,
+	} : undefined;
+
+	const plugins = [
+
+		new MiniCssExtractPlugin({
+			linkType: PATH.OUTPUT.CSS.TYPE,
+			filename: PATH.OUTPUT.CSS.NAME,
+			chunkFilename: PATH.OUTPUT.CSS.CHUCK,
+		}),
+
+		new HtmlWebpackPlugin(htmlWebpack),
+
+		new CopyPlugin({
+			patterns: [
+				{ from: PATH.INPUT.IMG, to: PATH.OUTPUT.IMG },
+			],
+		}),
+
+	];
+
+	if (MODE.isPRODUCTION) {
+		plugins.push(
+			new ImageminPlugin({
+				disable: true,
+				test: TEST.IMG,
+				optipng: { optimizationLevel: 3 },
+				jpegtran: { progressive: true },
+				gifsicle: { optimizationLevel: 1 },
+				svgo: {},
+			}),
+			new BundleAnalyzerPlugin({
+				analyzerMode: 'disabled',
+			}),
+			new BabelMinifyPlugin(),
+		);
+	}
+
+	return plugins;
+};
+
+// CSS LOADERS
+const cssLoaders = (preProcessor) => {
+	const loaders = [
+		{
+			loader: MiniCssExtractPlugin.loader,
+			options: {
+				publicPath: PATH.OUTPUT.CSS.PUBLIC,
+			},
+		},
+		{
+			loader: 'css-loader',
+			options: {},
+		},
+		{
+			loader: 'postcss-loader',
+			options: {},
+		},
+	];
+	if (preProcessor) {
+		loaders.push(preProcessor);
+	}
+	return loaders;
+};
+
+// MODULE RULES
+const getModuleRules = () => {
+	const rules = [
+		{
+			test: TEST.SCSS,
+			use: cssLoaders('sass-loader'),
+		},
+		{
+			test: TEST.IMG,
+			use: [
+				{
+					loader: 'file-loader',
+					options: {
+						name: '[name].[hash:8].[ext]',
+						outputPath: PATH.OUTPUT.MEDIA,
+					},
+				},
+			],
+		},
+		{
+			test: TEST.FONTS,
+			use: [
+				{
+					loader: 'file-loader',
+					options: {
+						name: '[name].[hash:8].[ext]',
+						outputPath: PATH.OUTPUT.FONTS,
+					},
+				},
+			],
+		},
+		{
+			test: TEST.JS,
+			exclude: /node_modules/,
+			use: {
+				loader: 'babel-loader',
+				options: {
+					presets: ['@babel/preset-env'],
+				},
+			},
+		},
+	];
+	return rules;
+};
+
+// SERVER
+const getServer = () => {
+	const server = {
+		watchContentBase: true,
+		compress: true,
+		port: 4001,
+		hot: true,
+		noInfo: true,
+	};
+	return server;
+};
+
+// ======================================================================================
+
+const CONFIG = {
+	mode: MODE.CURRENT,
+	devtool: MODE.isPRODUCTION ? 'source-map' : 'cheap-module-source-map',
+	entry: {
+		main: [PATH.ENTRY.BABEL, PATH.ENTRY.MAIN],
+	},
+	output: {
+		path: PATH.OUTPUT.DIR,
+		filename: PATH.OUTPUT.JS.NAME,
+		chunkFilename: PATH.OUTPUT.JS.CHUCK,
+	},
+	plugins: getPlugins(),
+	module: {
+		rules: getModuleRules(),
+	},
+	devServer: getServer(),
+};
+
+if (MODE.isPRODUCTION) {
+	CONFIG.output.publicPath = './';
+	CONFIG.optimization = {
+		splitChunks: {
+			chunks: 'all',
+		},
+		minimizer: [
+			new TerserWebpackPlugin(),
+			new OptimizeCssAssetsPlugin({
+				cssProcessorOptions: { discardComments: { removeAll: true } },
+			}),
+		],
+	};
+}
+
+// ======================================================================================
+
+module.exports = CONFIG;
+
+// ======================================================================================
